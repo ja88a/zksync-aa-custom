27e1529c07077c364e2eca82b5dadaf9daa3a8aa
ja88a <r0g3r@tuta.io>
2023-08-11 12:00:34 +0200

    enhanced hardhat config + dotenv integration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,46 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 
+import { config as dotenvConfig } from "dotenv";
+import { resolve } from "path";
+
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
+
+if (!process.env.DEPLOY_PRIVATE_KEY) {
+  throw new Error("DEPLOY_PRIVATE_KEY in .env not set");
+}
+
+const zkSyncTestnet =
+  process.env.NODE_ENV == "test"
+    ? {
+        url: "http://localhost:3050",
+        ethNetwork: "http://localhost:8545",
+        zksync: true,
+        accounts: [
+          // rich wallets from local-node https://github.com/matter-labs/local-setup/blob/main/rich-wallets.json
+          "0x77268...",
+          "0xac1e7...",
+        ],
+      }
+    : {
+        url: "https://zksync2-testnet.zksync.dev",
+        ethNetwork: "goerli",
+        zksync: true,
+        accounts: [
+          // account PKs loaded from .env file
+          process.env.DEPLOY_PRIVATE_KEY
+            ? process.env.DEPLOY_PRIVATE_KEY
+            : "env.DEPLOY_PK undefined",
+          process.env.USER_PRIVATE_KEY
+            ? process.env.USER_PRIVATE_KEY
+            : "env.USER_PK undefined",
+        ],
+        // contract verification endpoint
+        verifyURL:
+          "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+      };
+
 const config: HardhatUserConfig = {
   zksolc: {
     version: "latest",
@@ -15,11 +55,7 @@ const config: HardhatUserConfig = {
     hardhat: {
       zksync: true,
     },
-    zkSyncTestnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-      zksync: true,
-    },
+    zkSyncTestnet,
   },
   solidity: {
     version: "0.8.17",
